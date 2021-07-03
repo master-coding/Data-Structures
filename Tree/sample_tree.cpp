0c98fcab99a7e07cacee3d920ed35936a063df5d
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-03 10:22:48 +0530

    inorder preorder postorder traversals

--- Tree/sample_tree.cpp
+++ Tree/sample_tree.cpp
@@ -61,6 +61,8 @@ int main() {
     root -> left = new Node(2);
     root -> right = new Node(3);
     root -> left -> right = new Node(4);
+    root -> right -> left = new Node(5);
+    root -> right -> right = new Node(6);
 
     cout << "Inorder traversal "; inorder(root); newline;
     cout << "Preorder traversal "; preorder(root); newline;
