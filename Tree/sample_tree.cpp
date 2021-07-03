798fa6e4abd0a39e651a738dd2bffca677d435f6
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-03 10:11:09 +0530

    inorder traversal add

--- Tree/sample_tree.cpp
+++ Tree/sample_tree.cpp
@@ -13,9 +13,20 @@ struct Node {
     }
 };
 
+// inorder follows LEFT ROOT RIGHT
+void inorder(Node *root) {
+    if (root != NULL) {
+        inorder(root -> left);
+        cout << root -> key << " ";
+        inorder(root -> right);
+    }
+}
+
 int main() {
     Node *root = new Node(1);  // creating a root node with value 1
     root -> left = new Node(2);
     root -> right = new Node(3);
     root -> left -> right = new Node(4);
+
+    inorder(root);
 }
