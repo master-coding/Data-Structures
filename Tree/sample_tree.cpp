d6f6fc2af5a9482b2d9803b9261cefd2ac6ad6a6
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-03 10:19:38 +0530

    add inorder preorder postorder

--- Tree/sample_tree.cpp
+++ Tree/sample_tree.cpp
@@ -1,8 +1,24 @@
 // sample tree implementation
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
 
 struct Node {
     int key;
@@ -22,11 +38,31 @@ void inorder(Node *root) {
     }
 }
 
+// preorder follows ROOT LEFT RIGHT
+void preorder(Node *root) {
+    if (root != NULL) {
+        cout << root -> key << " ";
+        preorder(root -> left);
+        preorder(root -> right);
+    }
+}
+
+// postorder follows LEFT RIGHT ROOT  
+void postorder(Node *root) {
+    if (root != NULL) {
+        postorder(root -> left);
+        postorder(root -> right);
+        cout << root -> key << " ";        
+    }
+}
+
 int main() {
     Node *root = new Node(1);  // creating a root node with value 1
     root -> left = new Node(2);
     root -> right = new Node(3);
     root -> left -> right = new Node(4);
 
-    inorder(root);
+    cout << "Inorder traversal "; inorder(root); newline;
+    cout << "Preorder traversal "; preorder(root); newline;
+    cout << "Postorder traversal "; postorder(root); newline;
 }
