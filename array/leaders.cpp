244bf245a5d3898a23834a4b625862bc0de9d0ed
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-05 12:10:30 +0530

    leaders in an array

--- array/leaders.cpp
+++ array/leaders.cpp
@@ -6,21 +6,8 @@
 using namespace std;
 
 #define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define tlp int tc; cin >> tc; while (tc--)
-#define rep(i, a, b) for(int i = a; i < b; ++i)
 #define sp ' '
 #define nl '\n'
-#define newline cout << '\n'
-#define ll long long int
-
-#define vi vector <int>
-#define pii pair <int, int>
-#define pb push_back
-#define pob pop_back
-#define f first
-#define s second
-#define yes cout << "YES" << nl
-#define no cout << "NO" << nl
 
 void leaders(int arr[], int n) {
     int leader = arr[n - 1];
