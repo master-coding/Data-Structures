ac542752260fc1ac8d4c54398c7ce7b35bb3f8c5
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-05 12:19:19 +0530

    frequencies of elements in array

---
+++ array/frequencies_array.cpp
@@ -0,0 +1,34 @@
+// frequencies of elements in an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define sp ' '
+#define nl '\n'
+
+
+void frequencies(int arr[], int n) {
+    int count = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            count++;
+        }
+        else {
+            cout << arr[i - 1] << " : " << count << nl;
+            count = 1;
+        }
+    }
+    cout << arr[n - 1] << " : " << count << nl;
+}
+
+int main() {
+    speedio;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    frequencies(a, n);
+}
