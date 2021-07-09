8eac6be182c368a95a1efae184d648aaa9758181
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-09 12:47:34 +0530

    insertion beginning end

--- Linked List/insertion.cpp
+++ Linked List/insertion.cpp
@@ -25,21 +25,50 @@ Node *insertBeginning(Node *head, int value) {
     return temp;
 }
 
+Node *insertEnd(Node *head, int value) {
+    Node *temp = new Node(value);
+    if (head == NULL)
+        return temp;
+    Node *curr = head;
+    while (curr -> next != NULL)
+        curr = curr -> next;
+    
+    curr -> next = temp;
+    return head;
+}
 
 int main() {
     speedio;
     
-    Node *head = new Node(1);
-    head -> next = new Node(2);
-    int times;
-    cin >> times;
-    cout << "Enter the number of nodes you want to add : " << times << "\n";
-    for (int i = 0; i < times; i++) {
-        int value;
-        cin >> value;
-        cout << "Value" << i + 1 << " : " << value << '\n';
-        head = insertBeginning(head, value);
+    Node *head = NULL;
+    cout << 1. << " Insert at the beginning \n";
+    cout << 2. << " Insert at the end \n";
+    cout << 3. << " Display the linked list \n";
+    cout << "  Any other key to exit \n";
+
+    while (true) {
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            int value;
+            cin >> value;
+            head = insertBeginning(head, value);
+        }
+        else if (choice == 2) {
+            int value;
+            cin >> value;
+            head = insertEnd(head, value);
+        }
+        else if (choice == 3) {
+            cout << "Linked list : " ;
+            displayList(head);
+            cout << "\n";
+        }
+        else {
+            cout << "Linked list : " ;
+            displayList(head);
+            cout << "\n";
+            break;
+        }
     }
-    cout << "Linked list : ";
-    displayList(head);
 }
