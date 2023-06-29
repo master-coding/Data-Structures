78d32e01a5fb7675ee891f0546e9539138b8e0b0
aaris-cogoport <aaris.ahmed@cogoport.com>
2023-06-29 18:53:31 +0530

    Maximum sum in the subarray

--- array/maxSumSubarray.cpp
+++ array/maxSumSubarray.cpp
@@ -1,30 +1,45 @@
-// find the maximum sum in the array possible
-// arr[] = {1, 2, 3, -8, 3}     8
-// arr[] = {1, 2, 3}    6
+// Problem: Maximum sum Subarray
+// Find the maximum sum from all the subarrays
+// eg: {1, 2, 3, -8, 3, 5}
 
 #include <iostream>
 using namespace std;
 
-void maxSumSubarray(int arr[], int n) {
-    int ans = arr[0], sum = arr[0];
-    if (n < 1) {
-        cout << "Array have 0 elements";
-        return;
+// time complexity: O(n**2)
+// this method creates all the subarrays and find the maximum sum in all
+int maxSumSubarrayMt1(int arr[], int n) {
+    int maxSum = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        int currSum = 0;
+        for (int j = i; j < n; j++) {
+            currSum += arr[j];
+            maxSum = max(maxSum, currSum);
+        }
     }
+
+    return maxSum;
+}
+
+
+// time complexity: O(n)
+// Uses Kadane's algo: Find the maximum subarray sum till that point (prevMaxSum + arr[i], arr[i])
+int maxSumSubarrayMt2(int arr[], int n) {
+    int maxSum = arr[0], maxSumSoFar = arr[0];
 
     for (int i = 1; i < n; i++) {
-        sum = max(arr[i], sum + arr[i]);
-        ans = max(sum, ans);
+        maxSumSoFar = max(maxSumSoFar + arr[i], arr[i]);
+        maxSum = max(maxSumSoFar, maxSum);
     }
-
-    cout << ans;
+    
+    return maxSum;
 }
 
 int main() {
     int n; cin >> n;
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    maxSumSubarray(arr, n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    // cout << maxSumSubarrayMt1(arr, n);
+    cout << maxSumSubarrayMt2(arr, n);
 }
